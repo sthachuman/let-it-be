af3e079d2ccc18712715ad80860dc43cd499aa9e
Sajan <sajansinghthakuri0@gmail.com>
2023-10-04 09:00:11 +0545

    Logo

--- client/src/components/LogoSearch/LogoSearch.jsx
+++ client/src/components/LogoSearch/LogoSearch.jsx
@@ -5,7 +5,7 @@ import { UilSearch } from '@iconscout/react-unicons'
 const LogoSearch = () => {
   return (
     <div className="LogoSearch">
-      <img src={Logo} alt="" />
+      <img src={Logo} style={"height:40px; width:40px;"} alt="" />
       <div className="Search">
           <input type="text" placeholder="#Explore"/>
           <div className="s-icon">
