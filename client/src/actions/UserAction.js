1fa431a26053cf170ec1401d5ae92b5594379d3a
minuschuman <sthachuman@gmail.com>
2023-10-09 06:50:08 +0545

    feat enable chat

--- client/src/actions/UserAction.js
+++ client/src/actions/UserAction.js
@@ -1,4 +1,5 @@
 import * as UserApi from "../api/UserRequests";
+import {createChat, findChat} from "../api/ChatRequests";
 
 
 export const updateUser=(id, formData)=> async(dispatch)=> {
@@ -17,6 +18,19 @@ export const updateUser=(id, formData)=> async(dispatch)=> {
 export const followUser = (id, data)=> async(dispatch)=> {
     dispatch({type: "FOLLOW_USER", data: id})
     UserApi.followUser(id, data)
+    console.log(data._id,id)
+    const {data: chat} = await findChat(id, data._id)
+    const {data: chat2} = await findChat(data._id, id)
+    if(chat){
+        console.log("Chat already exists")
+    }
+    else if(chat2){
+        console.log("Chat already exists")
+    }
+    else{
+        console.log("Chat doesn't exist")
+        createChat({senderId: id, receiverId: data._id})
+    }
 }
 
 export const unfollowUser = (id, data)=> async(dispatch)=> {
